95084ffa8312257c47d302127d324756fe3e2e8f
Josiah Ritchie <josiah@josiahritchie.com>
2014-03-12 16:14:56 -0400

    Abstracting the Sine pulse
    Abstracting the sine pulse code to a function and making room for some
    alternate pulse, fade, etc. styles

--- Trinket_MoodLampPulsing/Trinket_MoodLampPulsing.ino
+++ Trinket_MoodLampPulsing/Trinket_MoodLampPulsing.ino
@@ -24,14 +24,17 @@ int pause = 500;
 
 // the setup routine runs once when you press reset:
 void setup() {                
+  // initialize pin 4's PWM capability
+  PWM4_init();
+
   // initialize the digital pin as an output.
   pinMode(redLed, OUTPUT);     
   pinMode(greenLed, OUTPUT);     
   pinMode(blueLed, OUTPUT);
   pinMode(button, INPUT);
-  
-  // initialize pin 4's PWM capability
-  PWM4_init();
+  analogWr(redLed, 0);
+  analogWr(greenLed, 0);
+  analogWr(blueLed, 0);
 }
 
 // the loop routine runs over and over again forever:
@@ -40,25 +43,9 @@ void loop() {
   
   if (buttonState == HIGH) { //if the button state is on, pulse the lights
 
-    float in, out;
-  
-    for (in = 0; in < 6.283; in = in + 0.001)
-    {
-      out = sin(in) * 127.5 + 127.5;
-      analogWr(redLed,out);
-    }
-
-    for (in = 0; in < 6.283; in = in + 0.001)
-    {
-      out = sin(in) * 127.5 + 127.5;
-      analogWr(greenLed,out);
-    }
-
-    for (in = 0; in < 6.283; in = in + 0.001)
-    {
-      out = sin(in) * 127.5 + 127.5;
-      analogWr(blueLed,out);
-    }
+    pulse(redLed);
+    pulse(greenLed);
+    pulse(blueLed);
 
   } else { // if the button state is off
 
@@ -70,14 +57,27 @@ void loop() {
   }
 }
 
-void PWM4_init() {
+// Pulse an LED using a sin wave cause it's pretty 
+void pulse(int pin) {
+  float in,out;
+  
+  for (in = 0; in < 6.283; in = in + 0.001)
+  {
+    out = sin(in) * 127.5 + 127.5;
+    out = -(out - 255);
+    analogWr(pin,out);
+  }
+}
+
 // Set up PWM on Trinket GPIO #4 (PB4, pin 3) using Timer 1
+void PWM4_init() {
 TCCR1 = _BV (CS10); // no prescaler
 GTCCR = _BV (COM1B1) | _BV (PWM1B); // clear OC1B on compare
 OCR1B = 127; // duty cycle initialize to 50%
 OCR1C = 255; // frequency
 }
 
+// A single function to handle PWM any pin
 void analogWr(int pin, uint8_t value) {
   if (pin == 4) { 
     OCR1B = value; // The PWM duty value between 0 and 255 (0 to 100%)
