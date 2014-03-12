34b8f5b9fcbb68eb35865ff82108a94f479ed340
Josiah Ritchie <josiah@josiahritchie.com>
2014-03-12 16:26:23 -0400

    Update Trinket_MoodLampSmooth.ino
    Getting the pin4 stuff setup.

--- Trinket_MoodLampSmooth/Trinket_MoodLampSmooth.ino
+++ Trinket_MoodLampSmooth/Trinket_MoodLampSmooth.ino
@@ -10,9 +10,21 @@ int redLed = 0;
 int greenLed = 1;
 int blueLed = 4;
 
- 
-void setup_smooth_colors() 
+
+void setup() 
 { 
+
+  // initialize pin 4's PWM capability
+  PWM4_init();
+
+  // initialize the digital pin as an output.
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  pinMode(blueLed, OUTPUT);
+
+  analogWr(redLed, 0);
+  analogWr(greenLed, 0);
+  analogWr(blueLed, 0);
 
   randomSeed(analogRead(0));
   
@@ -23,7 +35,7 @@ void setup_smooth_colors()
 
 } 
  
-void smooth_colors() 
+void loop() 
 { 
   randomSeed(analogRead(0));
   
@@ -51,5 +63,23 @@ void smooth_colors()
   RGB2[x] = constrain(RGB2[x], 0, 255); 
   
   delay(1000);
+ }
+}
+
+
+// Set up PWM on Trinket GPIO #4 (PB4, pin 3) using Timer 1
+void PWM4_init() {
+TCCR1 = _BV (CS10); // no prescaler
+GTCCR = _BV (COM1B1) | _BV (PWM1B); // clear OC1B on compare
+OCR1B = 127; // duty cycle initialize to 50%
+OCR1C = 255; // frequency
+}
+
+// A single function to handle PWM any pin
+void analogWr(int pin, uint8_t value) {
+  if (pin == 4) {
+    OCR1B = value; // The PWM duty value between 0 and 255 (0 to 100%)
+ } else {
+    analogWrite(pin, value);
  }
 }
